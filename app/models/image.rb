56ee4146a96b7ca034a081bc8071ebd3ce7f4ad4
Andrew <arolson56@gmail.com>
2017-11-18 17:10:36 -0800

    Add AWS

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,10 @@ class Image < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "Should be less than 5MB")
+    end
+  end
 end
